3d90c2116390e476b4e75b14b0f351eff527d5a2
Tommy Dang <quantumventuress@gmail.com>
2014-08-16 15:48:48 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tommydangerous::Application.config.secret_key_base = '8bbbb4f73d991a8b925b5912820fdf76228110acbe9be7f8745b9df9998eb79b2714a6deeaa2488d2e2375fe2e07e812a92329068ddda6628510f98f9b03405b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tommydangerous::Application.config.session_store :cookie_store, key: '_tommydangerous_session'
